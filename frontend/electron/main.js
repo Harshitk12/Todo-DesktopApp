167ac2437ac5f26ce7fec477a188b72e04f5a50a
Harshit Kushwaha <harshitk5454@gmail.com>
2026-07-08 16:07:25 +0530

    made some changes to build electron app easily : moved electron folder inside frontend and changed base url in vite.config.ts , added some configuration to package.json related to build .Finally updated the readme

---
+++ frontend/electron/main.js
@@ -0,0 +1,21 @@
+import { app, BrowserWindow } from "electron";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 700,
+  });
+
+  if (app.isPackaged) {
+    win.loadFile(path.join(__dirname, "../dist/index.html"));
+  } else {
+    win.loadURL("http://localhost:5173");
+  }
+}
+
+app.whenReady().then(createWindow);
